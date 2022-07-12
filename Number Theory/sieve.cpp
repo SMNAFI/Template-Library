337be0f86621ba6296d8d2546e88f942b4f495f5
SMNAFI <sayem.md.nafi@gmail.com>
2022-07-12 12:51:14 +0600

    sieve of eratosthenes

---
+++ Number Theory/sieve.cpp
@@ -0,0 +1,15 @@
+
+const int N = 10000005;
+bitset<N> prime;
+
+void sieve() {
+    for(int i = 4; i <= N; i += 2) prime[i] = true;
+
+    for(int i = 3; i * i <= N; i += 2) {
+        if(!prime[i]) {
+            for (int j = i * i; j <= N; j += i * 2) {
+                prime[j] = true;
+            }
+        }
+    }
+}
