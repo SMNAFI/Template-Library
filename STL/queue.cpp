3f7d80710cbe29357df9ec43cffe2322bb70bf16
SMNAFI <sayem.md.nafi@gmail.com>
2022-07-31 01:09:59 +0600

    queue

---
+++ STL/queue.cpp
@@ -0,0 +1,13 @@
+
+queue<type> q;
+
+// Member functions
+
+    st.push();      // O(logN)
+    st.pop();       // O(logN)
+
+    st.front();      // O(1)
+    st.back();      // O(1)
+    
+    st.empty();     // O(1)
+    st.size();      // O(1)
