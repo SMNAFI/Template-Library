261d4d51b4f94e1ad757021d0777f1ddbf8506b2
SMNAFI <sayem.md.nafi@gmail.com>
2022-07-31 01:03:26 +0600

    stack

---
+++ STL/stack.cpp
@@ -0,0 +1,10 @@
+
+stack<type> st;
+
+// Member functions
+
+    st.push();      // O(logN)
+    st.pop();       // O(logN)
+    st.top();       // O(1)
+    st.empty();     // O(1)
+    st.size();      // O(1)
